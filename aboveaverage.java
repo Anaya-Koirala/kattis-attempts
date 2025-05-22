6c7f6c5d9052256f041ef1cb3a7b9b8f3e39e243
Anaya Koirala <anaya@koirala.xyz>
2025-05-22 15:44:36 -0700

    New Content

---
+++ aboveaverage.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+I feel like a FP wizard with this code.
+For reference, I spent 1 week tryna learn haskell for a stupid pandoc project
+that I realized I had no use for that later on
+... and I still don't like haskell
+*/
+
+public class aboveaverage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int dataset = input.nextInt();
+        while (dataset != 0) {
+            input.nextInt(); // Don't need to class size
+
+            double[] grades = Arrays.stream(input.nextLine().trim().split(" "))
+                    .mapToDouble(Double::parseDouble)
+                    .toArray();
+
+            double average = Arrays.stream(grades)
+                    .average()
+                    .getAsDouble();
+
+            long aboveAverage = Arrays.stream(grades)
+                   .filter(grade -> grade > average)
+                   .count();
+
+            double aboveAveragePercentage = (double)aboveAverage / grades.length * 100;
+            System.out.printf("%.3f%%\n",aboveAveragePercentage);
+
+            dataset -= 1;
+        }
+        input.close();
+    }
+}
